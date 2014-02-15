de38625f6d8953e3a4cc4b3fd478b4990bfb4cb0
sccalabr <sccalabr@calpoly.edu>
2014-02-14 22:33:46 -0600

    Update memSim.h

--- memSim.h
+++ memSim.h
@@ -6,17 +6,42 @@
 #define PAGE_SIZE 256
 #define FRAME_SIZE 256
 
-typedef struct NEED_TO_THINK_OF_A_GOOD_NAME{
-   int logicalMemoryPageNum;
-   int physicalMemoryFrameNum;
-} NEED_TO_THINK_OF_A_GOOD_NAME;
+typedef struct PageAndFrameNumber{
+   int pageNum;
+   int frameNum;
+} PageAndFrameNumber;
 
 
 typedef struct Node{
    struct Node *prev;
    struct Node *next;
-   struct NEED_TO_THINK_OF_A_GOOD_NAME *thing;
+   struct PageAndFrameNumber *data;
 } Node;
+
+typedef struct MemBlock{
+   int frameNum;
+   char data[256];
+   struct MemBlock *next;
+   struct MemBlock *prev;
+} MemBlock;
+
+Node *checkIfInTLB(unsigned int pageNumber);
+
+Node *checkIfInPageTable(unsigned int pageNumber);
+
+void makeNewTLBNode(unsigned int pageNumber, unsigned int frameNumber);
+
+void makeNewPageTableNode(unsigned int pageNumber);
+
+void makeNewMemBlock(unsigned int page);
+
+void removeAndFreeStartNodeTLB();
+
+void removeAndFreeStartMemBlock();
+
+void loadFrame(unsigned int page);
+
+void findByte(Node *inPageTable, unsigned int offset);
 
 void noReplacement();
 
